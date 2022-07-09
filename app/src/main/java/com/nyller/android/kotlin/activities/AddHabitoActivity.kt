5a2e640cbb3813416c87d9f8ad2234b087149928
Nyller <edu.sampaio1777@gmail.com>
2022-07-09 12:32:57 -0300

    Versão com escrita no Banco de dados e Leitura dos dados com exibição em RecyclerView

--- app/src/main/java/com/nyller/android/kotlin/activities/AddHabitoActivity.kt
+++ app/src/main/java/com/nyller/android/kotlin/activities/AddHabitoActivity.kt
@@ -1,10 +1,13 @@
 package com.nyller.android.kotlin.activities
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import com.google.firebase.firestore.FirebaseFirestore
+import com.nyller.android.kotlin.MainActivity
 import com.nyller.android.kotlin.R
 import kotlinx.android.synthetic.main.activity_add_habito.*
 import kotlinx.android.synthetic.main.adapter_habito.*
@@ -20,16 +23,20 @@ class AddHabitoActivity : AppCompatActivity() {
         btnSalvar.setOnClickListener(View.OnClickListener {
 
             val usuariosMap = hashMapOf(
-                "name" to editNome.text.toString(),
-                "turn" to editTurno.text.toString(),
-                "category" to editCategoria.text.toString())
+                "nome" to editNome.text.toString(),
+                "turno" to editTurno.text.toString(),
+                "categoria" to editCategoria.text.toString())
 
-            db.collection("meus_habitos").document("Edu")
-                .set(usuariosMap).addOnCompleteListener {
+            db.collection("habitos").add(usuariosMap)
+                .addOnCompleteListener {
                     Log.i("Db Firestore", "Sucesso ao salvar dados!")
                 }.addOnFailureListener {
                     Log.i("Erro Firestore", "Erro ao salvar dados!")
                 }
+        })
+
+        btnAbrir.setOnClickListener(View.OnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
         })
 
     }
